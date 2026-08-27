14afbac915f503668dd68abd9defc7bc387ad1f0
Kerby Martino <kerbymart@gmail.com>
2026-08-27 23:19:26 +0800

    test: cover configured handle release sessions

---
+++ TestHandleFixture.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+extern "C" __declspec(dllexport) unsigned long long CreateOwnedHandle()
+{
+    return static_cast<unsigned long long>(reinterpret_cast<uintptr_t>(
+        CreateEventA(nullptr, TRUE, FALSE, nullptr)));
+}
