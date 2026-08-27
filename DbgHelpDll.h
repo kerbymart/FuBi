8006476d86516f8f0cb2663c24d71ec95c4579ab
Kerby Martino <kerbymart@gmail.com>
2026-08-27 14:04:47 +0800

    feat: add validated local PDB symbol evidence

--- DbgHelpDll.h
+++ DbgHelpDll.h
@@ -8,6 +8,17 @@
 #pragma once
 
 #include <DbgHelp.h>
+#include "FunctionCatalog.h"
+
+#include <string>
+#include <vector>
+
+struct SymbolPrototypeEvidence
+{
+    uint32_t rva = 0;
+    std::string name;
+    PrototypeSpec prototype;
+};
 
 class DbgHelpDll
 {
@@ -18,4 +29,10 @@ public:
 	~DbgHelpDll(void);
 public:
 	DWORD UnDecorateSymbolName(const char* DecoratedName, char* UnDecoratedName, DWORD UndecoratedLength, DWORD Flags);
+	bool EnumerateExactFunctionSymbols(const std::string& imagePath,
+	    const ModuleIdentity& expected, std::vector<SymbolPrototypeEvidence>& symbols,
+	    std::string& error);
+
+private:
+    HMODULE handle_ = nullptr;
 };
